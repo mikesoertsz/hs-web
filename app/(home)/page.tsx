829fc8ec766379b24efb35b886c2f1272b08d78f
Mike Soertsz <mike@drifterconsulting.com>
2024-02-26 16:53:09 +0000

    hiding overview section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <Benefits />
       <HowGuaranteed />
       <WhoFor />
-      <Overview />
+      {/* <Overview /> */}
       <HowItWorks />
       <DetailsBlock />
       <IncomeDistribution />
