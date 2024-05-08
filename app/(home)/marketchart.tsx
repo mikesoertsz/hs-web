c04e13cc200068bcc822ec069574f1228c0d6d8e
Mike Soertsz <mike@drifterconsulting.com>
2024-05-08 09:52:02 +0100

    fixing the market graph numbers

--- app/(home)/marketchart.tsx
+++ app/(home)/marketchart.tsx
@@ -4,9 +4,13 @@ export function MarketChart() {
   return (
     <>
       <BarChart
-        xAxis={[{ scaleType: 'band', data: ['2018', '2023', '2024', '2032'] }]}
+        xAxis={[{ scaleType: "band", data: ["2018", "2023", "2027", "2032"] }]}
         series={[
-          { data: [6.5, 8.5, 8.5, 19.78], label: "Market Growth", color: "#CDC19B" }
+          {
+            data: [6.5, 8.5, 12.33, 19.78],
+            label: "Market Growth",
+            color: "#CDC19B",
+          },
         ]}
         width={800}
         height={550}
